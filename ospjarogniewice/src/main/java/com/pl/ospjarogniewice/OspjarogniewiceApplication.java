8bd45aff0bcba01797a1863f98b98746b838de96
Bartek <Bartek@Siegieda>
2021-03-13 12:22:17 +0100

    initial check-in of the ospjarogniewice code

---
+++ ospjarogniewice/src/main/java/com/pl/ospjarogniewice/OspjarogniewiceApplication.java
@@ -0,0 +1,13 @@
+package com.pl.ospjarogniewice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OspjarogniewiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OspjarogniewiceApplication.class, args);
+	}
+
+}
---
+++ ospjarogniewice/src/test/java/com/pl/ospjarogniewice/OspjarogniewiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.ospjarogniewice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OspjarogniewiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
